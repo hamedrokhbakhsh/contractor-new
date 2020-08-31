e6303aeb810258bc2138da33f3cf3a99ba6df82a
pedram poormehr <p.poormehr@gmail.com>
2020-08-31 19:17:04 +0430

    initialize and add

---
+++ src/app/pages/default/default.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default',
+  templateUrl: './default.page.html',
+  styleUrls: ['./default.page.scss'],
+})
+export class DefaultPage implements OnInit {
+
+  constructor() { }
+
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'پنل روزانه',
+      url: '/home',
+      icon: 'home'
+    }
+  ];
+
+  ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
+
+  logout() {
+    console.log('logout')
+  }
+}
